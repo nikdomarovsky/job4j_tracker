1a520fcb15a4dcb39f3f092de1957c5bbf1315cb
Администратор <nikdomarovsky@gmail.com>
2024-07-11 10:44:40 +0300

    6. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -6,23 +6,23 @@ public class Broken {
     public String surname;
 
     public static final String VALUE = "";
+    private String name;
 
-    void echo() {
+    public Broken() {
     }
 
-    void media(Object obj) {
+    public void echo() {
+    }
+
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
 
     }
 
-    String name;
-
-    Broken() {
-    }
 
 }
