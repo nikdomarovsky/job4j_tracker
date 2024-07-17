e95b255d1322ac31aba220a3a4662d3c3d74b80a
Администратор <nikdomarovsky@gmail.com>
2024-07-17 19:58:47 +0300

    1.7. Конструктор [#173889]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -4,9 +4,11 @@ public class Error {
     private boolean active;
     private int status;
     private String message;
-    public Error(){
+
+    public Error() {
     }
-    public Error(boolean active,int status,String message){
+
+    public Error(boolean active, int status, String message) {
         this.active = active;
         this.status = status;
         this.message = message;
@@ -15,14 +17,15 @@ public class Error {
     public static void main(String[] args) {
         Error error = new Error();
         error.printInfo();
-        Error error1 = new Error(true,404,"Not found");
+        Error error1 = new Error(true, 404, "Not found");
         error1.printInfo();
         Error error2 = new Error(false, 200, "OK");
         error2.printInfo();
         Error error3 = new Error(true, 500, "Internal Server Error");
         error3.printInfo();
     }
-    public void printInfo(){
+
+    public void printInfo() {
         System.out.println("Активная ошибка: " + active);
         System.out.println("Статус: " + status);
         System.out.println("Сообщение: " + message);
