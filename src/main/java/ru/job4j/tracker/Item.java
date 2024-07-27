e23a8189ffa11d61df6c6286b9202c021a66d57c
Администратор <nikdomarovsky@gmail.com>
2024-07-27 16:01:16 +0300

    8. Date. Отображение даты. [#310017]8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,20 +1,25 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created;
 
     public Item() {
-
+        this.created = LocalDateTime.now();
     }
 
     public Item(String name) {
         this.name = name;
+        this.created = LocalDateTime.now();
     }
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+        this.created = LocalDateTime.now();
     }
 
     public int getId() {
@@ -31,5 +36,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
