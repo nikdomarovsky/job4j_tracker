967dbe25e291f1a736a8c6671f1551c90d07b18a
Администратор <nikdomarovsky@gmail.com>
2024-08-05 09:50:07 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -50,7 +50,7 @@ public class Tracker {
         return result;
     }
 
-    public boolean replace(int id,Item item) {
+    public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index != -1) {
             item.setId(id);
