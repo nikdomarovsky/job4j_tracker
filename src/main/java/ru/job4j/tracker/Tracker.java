1092d01faca85261ec687dbe6c617216d3371ebd
Администратор <nikdomarovsky@gmail.com>
2024-08-07 14:28:38 +0300

    5. Tracker - хранилище [#396 #507444]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -30,7 +30,7 @@ public class Tracker {
                 result[index++] = items[i];
             }
         }
-        return Arrays.copyOf(result,index);
+        return Arrays.copyOf(result, index);
     }
 
     private int indexOf(int id) {
