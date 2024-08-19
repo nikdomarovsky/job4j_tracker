cd11fe72e0429ef7b8a6104028bdfa9e27754a29
Администратор <nikdomarovsky@gmail.com>
2024-08-19 15:59:48 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

---
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+import ru.job4j.tracker.input.ConsoleInput;
+import ru.job4j.tracker.input.Input;
+
+public class ValidateInput extends ConsoleInput {
+
+    @Override
+    public int askInt(String question) {
+        boolean invalid = true;
+        int value = -1;
+        do {
+            try {
+                value = super.askInt(question);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Пожалуйста, введите корректные данные");
+            }
+        } while (invalid);
+        return value;
+    }
+}
