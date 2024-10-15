97ce4c2fac3cc667b47da39a2bb8f6353dbd10cd
Администратор <nikdomarovsky@gmail.com>
2024-10-15 14:15:27 +0300

    Пример к заданию

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -4,25 +4,30 @@ public class Computer {
     private boolean multiMonitor;
     private int ssd;
     private String cpu;
+
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
         this.cpu = cpu;
     }
+
     public Computer(int ssd, String cpu) {
         this.ssd = ssd;
         this.cpu = cpu;
     }
+
     public Computer(int ssd, String cpu, boolean multiMonitor) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
         this.cpu = cpu;
     }
+
     public Computer(boolean multiMonitor, double ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = (int) ssd;
         this.cpu = cpu;
     }
+
     public static void main(String[] args) {
         Computer computer = new Computer(true, 500, "Intel Core I7-10700K");
         computer.printInfo();
